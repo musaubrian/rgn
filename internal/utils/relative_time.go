c68abe78cc6901ed1cee136b351bde9c3404b3cc
musaubrian <musaubrian15@gmail.com>
2023-06-19 13:26:40 +0300

    feat: display relative time correctly

---
+++ internal/utils/relative_time.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"fmt"
+	"time"
+)
+
+func TimeInDays(t time.Duration) string {
+	var relTime string
+	h := int(t.Hours())
+	m := int(t.Minutes())
+	d := h / 24
+	hrs := h % 24
+
+	if hrs == 0 {
+		relTime = fmt.Sprintf("%d mins", m)
+		return relTime
+	}
+
+	relTime = fmt.Sprintf("%dd %dhrs", d, hrs)
+
+	return relTime
+}
